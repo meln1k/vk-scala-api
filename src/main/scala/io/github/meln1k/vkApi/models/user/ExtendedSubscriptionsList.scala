c2daecd0f192adbf2f49d4f8f4cd811327d95a4a
Nikita Melkozerov <n.melkozerov@gmail.com>
2014-12-23 19:21:31 +0500

    bugfix

--- src/main/scala/io/github/meln1k/vkApi/models/user/ExtendedSubscriptionsList.scala
+++ src/main/scala/io/github/meln1k/vkApi/models/user/ExtendedSubscriptionsList.scala
@@ -1,22 +1,10 @@
 package io.github.meln1k.vkApi.models.user
 
 import play.api.libs.json._
-import play.api.libs.json.Reads._
-import play.api.libs.functional.syntax._
 
-case class ExtendedSubscriptionsList(count: Int, items: Seq[Either[Community, User]])
+
+case class ExtendedSubscriptionsList(count: Int, items: Seq[ItemsWrapper])
 
 object ExtendedSubscriptionsList {
   implicit val extendedSubscriptionsListReads = Json.reads[ExtendedSubscriptionsList]
-
-  implicit val eitherCommunityOrUserReads: Reads[Either[Community, User]] = new Reads[Either[Community, User]] {
-    def reads(json: JsValue): JsResult[Either[Community, User]] = {
-      (json \ "type").as[String] match {
-        case "page" | "group" | "event" =>
-          JsSuccess(Left(json.as[Community]))
-        case "profile" =>
-          JsSuccess(Right(json.as[User]))
-      }
-    }
-  }
 }
---
+++ src/main/scala/io/github/meln1k/vkApi/models/user/ItemsWrapper.scala
@@ -0,0 +1,21 @@
+package io.github.meln1k.vkApi.models.user
+
+import play.api.libs.json.{JsSuccess, JsResult, JsValue, Reads}
+
+/**
+ * Created by nmelkozerov on 23/12/14.
+ */
+case class ItemsWrapper(data: Either[Community, User])
+
+object ItemsWrapper {
+  implicit val itemsWrapperReads: Reads[ItemsWrapper] = new Reads[ItemsWrapper] {
+    def reads(json: JsValue): JsResult[ItemsWrapper] = {
+      (json \ "type").as[String] match {
+        case "page" | "group" | "event" =>
+          JsSuccess(ItemsWrapper(Left(json.as[Community])))
+        case "profile" =>
+          JsSuccess(ItemsWrapper(Right(json.as[User])))
+      }
+    }
+  }
+}
