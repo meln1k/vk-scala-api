6154759e1938afe32096128e13ecf0978e1b1263
Nikita Melkozerov <n.melkozerov@gmail.com>
2015-01-07 16:59:39 +0500

    small refactoring

--- src/main/scala/io/github/meln1k/vkApi/models/users/ComplaintType.scala
+++ src/main/scala/io/github/meln1k/vkApi/models/users/ComplaintType.scala
@@ -1,8 +1,5 @@
 package io.github.meln1k.vkApi.models.users
 
-/**
- * Created by nmelkozerov on 26/12/14.
- */
 object ComplaintType extends Enumeration {
   type ComplaintType = Value
   val porn, spam, insult, advertisment = Value
