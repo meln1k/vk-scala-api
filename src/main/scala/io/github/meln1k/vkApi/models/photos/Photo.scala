eaf5f1acd5dd8a24798d7fe6eb99fe483c4ec25e
Nikita Melkozerov <n.melkozerov@gmail.com>
2015-01-07 17:03:45 +0500

    added Photo model

---
+++ src/main/scala/io/github/meln1k/vkApi/models/photos/Photo.scala
@@ -0,0 +1,40 @@
+package io.github.meln1k.vkApi.models.photos
+
+import play.api.libs.json.Json
+
+/** A photo object describes a photo and contains the following fields:
+  *
+  * @param id Photo ID.
+  * @param album_id Photo album ID.
+  * @param owner_id ID of the user or community that owns the photo (if the photo located in community).
+  *                 For photos, uploaded by community, id = 100.
+  * @param user_id ID of the user that uploaded the photo.
+  * @param photo_75 URL of image with maximum size 75x75px.
+  * @param photo_130 URL of image with maximum size 130x130px.
+  * @param photo_604 URL of image with maximum size 604x604px.
+  * @param photo_807 URL of image with maximum size 807x807px.
+  * @param photo_1280 URL of image with maximum size 1280x1024px.
+  * @param photo_2560 URL of image with maximum size 2560x2048px.
+  * @param width Width (in pixels) of the original photo.
+  * @param height Height (in pixels) of the original photo.
+  * @param text Text describing the photo.
+  * @param date Date (in Unix time) the photo was added.
+  */
+case class Photo(id: Int,
+                 album_id: Int,
+                 owner_id: Int,
+                 user_id: Int,
+                 photo_75: String,
+                 photo_130: String,
+                 photo_604: String,
+                 photo_807: String,
+                 photo_1280: String,
+                 photo_2560: String,
+                 width: Int,
+                 height: Int,
+                 text: String,
+                 date: Long)
+
+object Photo {
+  implicit val photoReads = Json.reads[Photo]
+}
