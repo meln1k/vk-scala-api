02ee0a6621fd7c7977afebbeceb774a327741a87
Nikita Melkozerov <n.melkozerov@gmail.com>
2015-01-11 01:25:50 +0500

    UsersSpec refactoring

--- src/test/scala/UsersSpec.scala
+++ src/test/scala/UsersSpec.scala
@@ -3,13 +3,15 @@ import io.github.meln1k.vkApi.models.users._
 import io.github.meln1k.vkApi.services.PlayWSHttpLayerService
 import io.github.meln1k.vkApi.utils.{FakeAccessToken, RealAccessToken, ApiError}
 import org.specs2.mutable._
+import org.specs2.time.NoTimeConversions
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class UsersSpec extends Specification {
+class UsersSpec extends Specification with NoTimeConversions {
   implicit val testToken = RealAccessToken("66caf3a8a168a24755695a644492a41d4de2a5e117acc36b23920593bbe3dd7eeeb691b4a615d45dd9c46")
   val users = new Users with PlayWSHttpLayerService
+  val timeout = 5.second
   sequential
   "Users methods" should {
     "retrieve user profile with selected fields in users.get" in {
@@ -24,43 +26,43 @@ class UsersSpec extends Specification {
         counters,
         status
       ), nameCase = Some(nom))
-      Await.result(user, 2000 milli) must beAnInstanceOf[Seq[User]]
+      Await.result(user, timeout) must beAnInstanceOf[Seq[User]]
     }
 
     "find users by some criteria" in {
       val user  = users.search(query = Some("Vasya Babich"))
-      Await.result(user, 2000 milli) must beAnInstanceOf[UserList]
+      Await.result(user, timeout) must beAnInstanceOf[UserList]
     }
 
     "throw an exeption when searching without read accessToken" in {
       val usersWOToken = new Users()(FakeAccessToken) with PlayWSHttpLayerService
       val user  = usersWOToken.search(query = Some("Vasya Babich"))
-      Await.result(user, 2000 milli) must throwA[ApiError]
+      Await.result(user, timeout) must throwA[ApiError]
     }
 
     "tell if current user is app user" in {
       val isAppUser = users.isAppUser()
-      Await.result(isAppUser, 2000 milli) must haveClass[java.lang.Boolean]
+      Await.result(isAppUser, timeout) must haveClass[java.lang.Boolean]
     }
 
     "get extended user subscriptions" in {
       val subscriptions = users.getSubscriptions(userId = Some(3), count = Some(21))
-      Await.result(subscriptions, 2000 milli) must beAnInstanceOf[SubscriptionsList]
+      Await.result(subscriptions, timeout) must beAnInstanceOf[SubscriptionsList]
     }
 
     "get followers" in {
       val followers = users.getFollowers(userId = Some(1), count = Some(10))
-      Await.result(followers, 2000 milli) must beAnInstanceOf[UserList]
+      Await.result(followers, timeout) must beAnInstanceOf[UserList]
     }
 
     "report" in {
       val reportStatus = users.report(100, ComplaintType.spam)
-      Await.result(reportStatus, 2000 milli) must be equalTo(1)
+      Await.result(reportStatus, timeout) must be equalTo(1)
     }
 
     "find someone is some area" in {
       val foundUsers = users.getNearby(55.414327, 37.90047)
-      Await.result(foundUsers, 2000 milli) must beAnInstanceOf[UserList]
+      Await.result(foundUsers, timeout) must beAnInstanceOf[UserList]
     }
   }
 }
