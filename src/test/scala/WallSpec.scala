54478f0acf089fc9b6d5445618825c321d906b9d
Nikita Melkozerov <n.melkozerov@gmail.com>
2015-01-22 10:36:03 +0500

    wall specs

---
+++ src/test/scala/WallSpec.scala
@@ -0,0 +1,14 @@
+import io.github.meln1k.vkApi.methods.Wall
+import io.github.meln1k.vkApi.models.wall.{Post, PostList}
+import io.github.meln1k.vkApi.services.PlayWSHttpLayerService
+import io.github.meln1k.vkApi.utils.RealAccessToken
+import org.specs2.mutable.Specification
+import org.specs2.time.NoTimeConversions
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class WallSpec extends VkSpecification {
+
+
+}
