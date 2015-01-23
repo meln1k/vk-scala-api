e004a3ec103a4b80353f1d795ada0632f546ac61
Nikita Melkozerov <n.melkozerov@gmail.com>
2015-01-23 10:38:35 +0500

    wall methods class

---
+++ src/main/scala/io/github/meln1k/vkApi/methods/Wall.scala
@@ -0,0 +1,18 @@
+package io.github.meln1k.vkApi.methods
+
+
+import io.github.meln1k.vkApi.models.geo.{Longitude, Latitude}
+import io.github.meln1k.vkApi.models.wall.{PostId, Post, PostList}
+import io.github.meln1k.vkApi.models.wall.WallFilter._
+import io.github.meln1k.vkApi.services.HttpLayerService
+import io.github.meln1k.vkApi.utils.AccessToken
+import io.github.meln1k.vkApi.utils.ToStringConverters._
+import io.github.meln1k.vkApi.utils.ApiFutureUtils.FutureOps
+import io.github.meln1k.vkApi.utils.ToStringConverters._
+import scala.concurrent.Future
+
+
+class Wall(implicit accessToken: AccessToken) { this: HttpLayerService =>
+
+  
+}
