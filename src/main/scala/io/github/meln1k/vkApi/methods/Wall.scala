22b8d7a015452e0420f6c40050bfaae1988c182f
Nikita Melkozerov <n.melkozerov@gmail.com>
2015-02-12 12:17:24 +0500

    wall.get

--- src/main/scala/io/github/meln1k/vkApi/methods/Wall.scala
+++ src/main/scala/io/github/meln1k/vkApi/methods/Wall.scala
@@ -2,7 +2,7 @@ package io.github.meln1k.vkApi.methods
 
 
 import io.github.meln1k.vkApi.models.geo.{Longitude, Latitude}
-import io.github.meln1k.vkApi.models.wall.{PostId, Post, PostList}
+import io.github.meln1k.vkApi.models.wall.{PostId, Post, PostsList}
 import io.github.meln1k.vkApi.models.wall.WallFilter._
 import io.github.meln1k.vkApi.services.HttpLayerService
 import io.github.meln1k.vkApi.utils.AccessToken
@@ -14,5 +14,19 @@ import scala.concurrent.Future
 
 class Wall(implicit accessToken: AccessToken) { this: HttpLayerService =>
 
-  
+  def get(ownerId: Long = 0,
+          domain: Option[String] = None,
+          offset: Int = 0,
+          count: Int = 20,
+          filter: WallFilter = all,
+          extended: Boolean = false): Future[PostsList] = {
+    apiRequest("wall.get", Vector(
+      "owner_id" -> ownerId.toString,
+      "domain" -> domain,
+      "offset" -> offset.toString,
+      "count" -> count.toString,
+      "filter" -> filter.toString,
+      "extended" -> extended
+    )).map2[PostsList]
+  }
 }
