7d7e9376b15e11cc881a3bf797bfbf59830f8431
Nikita Melkozerov <n.melkozerov@gmail.com>
2015-02-18 22:58:45 +0500

    added note scaladoc

--- src/main/scala/io/github/meln1k/vkApi/models/notes/Note.scala
+++ src/main/scala/io/github/meln1k/vkApi/models/notes/Note.scala
@@ -2,7 +2,17 @@ package io.github.meln1k.vkApi.models.notes
 
 import play.api.libs.json.Json
 
-
+/**
+ * A note object describes a note and contains the following fields:
+ * @param id Note ID. positive number
+ * @param owner_id Note owner ID. positive number
+ * @param title Note title.
+ * @param text Note text.
+ * @param date Date (in Unix time) when the note was created.
+ * @param comments Number of comments.
+ * @param read_comments Number of read comments (only if owner_id is the current user).
+ * @param view_url Address of web page to view note.
+ */
 case class Note(id: Long,
                 owner_id: Long,
                 title: String,
